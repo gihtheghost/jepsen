fbd3426929b0f18a8db824b60b5534519c046e80
gih <giovannagaeta15@gmail.com>
2026-02-13 11:50:55 -0300

    coloquei o treplica dentro dos nos mas ainda n consegui rodar ele

--- meuteste/src/meuteste/core.clj
+++ meuteste/src/meuteste/core.clj
@@ -1,19 +1,39 @@
 (ns meuteste.core
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
 
+
+(defn db
+  "Etcd DB for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing etcd" version))
+
+    (teardown! [_ test node]
+      (info node "tearing down etcd"))))
 
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
-  :concurrency, ...), constructs a test map."
+  :concurrency ...), constructs a test map."
   [opts]
   (merge tests/noop-test
-         {:pure-generators true}
-         opts))
+         opts
+         {:name "etcd"
+          :os   debian/os
+          :db   (db "v3.1.5")
+          :pure-generators true}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
   [& args]
-  (cli/run! (cli/single-test-cmd {:test-fn etcd-test})
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test})
+                   (cli/serve-cmd))
             args))
