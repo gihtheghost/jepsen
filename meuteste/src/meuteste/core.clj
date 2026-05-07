b211688be5168f7ed4b8fccaf6cf91ae0d96c3a9
gih <giovannagaeta15@gmail.com>
2026-05-07 20:28:32 -0300

    servidor esta rodando quando roda lein run test

--- meuteste/src/meuteste/core.clj
+++ meuteste/src/meuteste/core.clj
@@ -11,24 +11,39 @@
 
 (def dir     "/opt/treplicadb")
 (def pidfile (str dir "/treplicadb.pid"))
+(def logfile (str dir "/server.log"))
 
 (defn db 
-  "Treplica DB for a particular version." ;isso aqui n vai dar certo pq n roda em backgroung, precisa de entrada do terminal
+  "Treplica DB for a particular version." 
   []
   (reify db/DB
     (setup! [_ test node]
-      (info node "iniciando treplica")
-      (let [state-dir (str "opt/treplicadb/state/" node)] ; define um state/n1 para cada node
+      (info node "iniciando database-with-treplica")
+      (let [state-dir (str "/opt/treplicadb/state/" node)] ; define um state/n1 para cada node
         
         (c/su
          ; cria um state/n1 para cada node no volume compartilhado
          (c/exec :mkdir :-p state-dir)
-         (c/cd dir
-               (c/exec :java :-cp "dist/treplica-0.5.0.jar:lib/slf4j-api-2.0.17.jar:lib/slf4j-nop-2.0.17.jar" "src.main.br.unicamp.treplica.examples.ReplicatedMap"
-                       "5" "200" state-dir)))))
+         (c/exec :git :clone "https://github.com/gihtheghost/database-with-treplica.git")
+         (c/cd "/database-with-treplica"
+               (c/exec :ant :build))
+         (cu/start-daemon! 
+          {:logfile logfile
+           :pidfile pidfile
+           :chdir "/database-with-treplica"
+           :background? true
+           :make-pidfile? true
+           :match-executable? true
+           :exec "/usr/bin/java"}
+          "/usr/bin/java" "-cp" "build/classes:lib/*" "src.database.Server"
+          "5" "200" state-dir "6666")
+         )))
 
-    (teardown! [_ test node]
-      (info node "terminando treplica"))))
+    (teardown! [_ test node] 
+               (info node "terminando database-with-treplica")
+               (cu/stop-daemon! pidfile)
+               (c/su (c/exec :rm :-rf "/database-with-treplica")))))
+
 
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
